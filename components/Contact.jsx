89515f5c74bfabef0853ec24f2165048ec86b76d
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-09-21 22:53:18 +0800

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
               <a
                 target="_blank"
                 onClick={event => event.preventDefault()}
-                className='py-3 px-6 md:px-3 uppercase bg-[#23272A] text-white rounded text-sm hover:bg-gray-500 focus:outline-none'
+                className='py-3 px-6 md:px-3 uppercase bg-[#23272A] text-white rounded text-sm focus:outline-none' // hover:bg-gray-500
                 style={{ width: 'fit-content', opacity: 0.2 }}
               >
                 Download my resume
