bc2450fe5ac3eebc6bde63da2c761f2117e045b9
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-09-23 11:43:17 +0800

    Update resume.js

--- resources/resume.js
+++ resources/resume.js
@@ -1,3 +1,3 @@
-const resumeLink = "https://drive.google.com/file/d/1PSvVrBiRGoh0i0F663Ig7knyB09ZBBMQ/view?usp=sharing";
+const resumeLink = "https://drive.google.com/file/d/1AC2uGU06QFt08yl09mQ5TrdYA1Edk2mH/view?usp=sharing";
 
 export { resumeLink }
