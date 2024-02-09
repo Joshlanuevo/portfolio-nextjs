101cc710d56babf8c4fc0866388e5d333027ca75
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2024-02-09 14:31:57 +0800

    disabled "Download my resume" button

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -35,8 +35,9 @@ const Contact = () => {
             <Link href={resumeLink}>
               <a
                 target="_blank"
+                onClick={event => event.preventDefault()}
                 className='py-3 px-6 md:px-3 uppercase bg-[#23272A] text-white hover:bg-gray-500 rounded text-sm focus:outline-none'
-                style={{ width: 'fit-content' }}
+                style={{ width: 'fit-content', opacity: 0.2 }}
               >
                 Download my resume
               </a>
